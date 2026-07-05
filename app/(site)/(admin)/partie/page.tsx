f08174b432b4df199c09690abe7420d873bf9702
gaida-lisongo <inbtpkinshasa@gmail.com>
2026-07-05 10:59:12 +0200

    creating message service

---
+++ app/(site)/(admin)/partie/page.tsx
@@ -0,0 +1,10 @@
+export default async function PartiePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold">Page de gestion des parties</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        Cette page est en cours de développement. Veuillez patienter pour les futures mises à jour.
+      </p>
+    </div>
+  );
+}
